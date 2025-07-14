7ad3cfe4c843a4a1c1779b22ac5f23f6ec5a7245
aadrsh26288 <aadrshmishra0@gmail.com>
2025-07-14 23:06:17 +0530

    title change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import HomePage from "@/modules/Home";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-  <div>
-  <HomePage />  
-  </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>nexalyze</title>
+			</Head>
+
+			<HomePage />
+		</div>
+	);
 }
